285ecd6ee7828a6c040c887c301adfeb66622374
14H <do.duc.hieu@linecorp.com>
2024-05-11 22:55:01 +0700

    Update credential

--- build-logic/src/main/kotlin/plugins/CommonMppPublish.kt
+++ build-logic/src/main/kotlin/plugins/CommonMppPublish.kt
@@ -30,10 +30,8 @@ class CommonMppPublish : Plugin<Project> {
                         maven {
                             url = uri(publishExtension.url)
                             credentials {
-                                username = findProperty("gpr.user") as String?
-                                    ?: System.getenv("USERNAME_GITHUB")
-                                password = findProperty("gpr.token") as String?
-                                    ?: System.getenv("TOKEN_GITHUB")
+                                username = "hieu-dd"
+                                password = "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"
                             }
                         }
                     }
